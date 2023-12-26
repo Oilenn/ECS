42a33c197104a5a1c7b2f3771c98bef5648ab01c
Rostislav Elengaupt <grandluntik0@gmail.com>
2023-12-26 17:23:06 +0400

    deleted initsystem

--- Assets/Systems/EcsInitSystem.cs
+++ Assets/Systems/EcsInitSystem.cs
@@ -10,21 +10,6 @@ namespace Client {
         public ButtonsFolder buttons;
 
         public void Init (IEcsSystems systems) {
-            Debug.Log("Init");
-
-            ref var button1 = ref _poolButtons.Value.Add(_world.Value.NewEntity());
-            ref var button2 = ref _poolButtons.Value.Add(_world.Value.NewEntity());
-            ref var button3 = ref _poolButtons.Value.Add(_world.Value.NewEntity());
-            ref var button4 = ref _poolButtons.Value.Add(_world.Value.NewEntity());
-            
-            button1.Button = buttons.GetButton(0);
-            button2.Button = buttons.GetButton(1);
-            button3.Button = buttons.GetButton(2);
-            button4.Button = buttons.GetButton(3);
-            button1.Button.onClick.AddListener(OnClickButton);
-            button2.Button.onClick.AddListener(OnClickButton);
-            button3.Button.onClick.AddListener(OnClickButton);
-            button4.Button.onClick.AddListener(OnClickButton);
 
         }
 
